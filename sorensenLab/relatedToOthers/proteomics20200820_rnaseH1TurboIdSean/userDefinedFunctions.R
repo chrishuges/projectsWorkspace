d102a3692ad3b47f3a7a61f38395a6742c35b428
Christopher Hughes <chrishuges@gmail.com>
2020-12-14 20:17:46 -0800

    old changes

--- sorensenLab/relatedToOthers/proteomics20200820_rnaseH1TurboIdSean/userDefinedFunctions.R
+++ sorensenLab/relatedToOthers/proteomics20200820_rnaseH1TurboIdSean/userDefinedFunctions.R
@@ -1,18 +1,8 @@
 #######################################################################
 #######################################################################
-##this function goes through the provided quant files and returns
-##a parsed object for each. It returns signal to noise ratio for quant
+##
 #######################################################################
-combineQuantFiles = function(filePath, ...){
-  quantData = read_tsv(filePath) %>%
-    dplyr::select(MS2ScanNumber, `126SignalToNoise`:`131CSignalToNoise`)
-  colnames(quantData) = c('scan','tmt10plex_126','tmt10plex_127N','tmt10plex_127C','tmt10plex_128N',
-                  'tmt10plex_128C','tmt10plex_129N','tmt10plex_129C','tmt10plex_130N','tmt10plex_130C','tmt10plex_131N','tmt10plex_131C')
-  ##
-  print(paste('Processing file ', filePath, sep = ''))
-  ##
-  return(quantData)
-}
+
 #######################################################################
 #######################################################################
 
