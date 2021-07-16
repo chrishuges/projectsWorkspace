871395bd477c1a7a9d9bfe7c292ac87d8155fecb
chrishuges <chrishuges@gmail.com>
2021-07-15 17:01:18 -0700

    code updates

---
+++ globalScripts/baseScript_rnaSeqSalmonStar.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+salmonLocation="/home/chughes/softwareTools/salmon-1.5.1/bin/salmon"
+indexLocation="/home/chughes/databases/refgenieManualGenomes/hg38/salmonPartialSaIndex_072021/default/"
+starLocation="/home/chughes/softwareTools/STAR-2.7.1a/bin/Linux_x86_64/STAR"
+referenceLocation="/home/chughes/databases/refgenieManualGenomes/hg38/starIndex_072021/default/"
+annotationLocation="/home/chughes/databases/refgenieManualGenomes/hg38/gencodeGtf_072021/default/2230c535660fb4774114bfa966a62f823fdb6d21acf138d4.gtf.gz"
+samtoolsLocation="/home/chughes/softwareTools/samtools-1.12/samtools"
+rawDataOutputDirectory="/mnt/Data/chughes/projectsRepository/sorensenLab/relatedToDlg2/sequencing20210713_a673DoxTreatmentPmid34009296/"
+
+#download the data files
+if [ ! -d $rawDataOutputDirectory ]; then
+  printf "Data directory $rawDataOutputDirectory does not exist, creating it.\n"
+  eval "mkdir $rawDataOutputDirectory"
+else
+  printf "Data directory $rawDataOutputDirectory exists, moving on.\n"
+fi
+
+eval "cd ${rawDataOutputDirectory}"
+eval "mkdir ${rawDataOutputDirectory}starResults"
+
+#process the downloaded files
+for i in SRR8561337 SRR8561334 SRR8561333 SRR8561335 SRR8561347 SRR8561332 SRR11908086 SRR11908085 SRR11908084 SRR11908082 SRR11908083 SRR11908081
+do
+  echo $i
+  
+  ##
+  salmonCall="$salmonLocation quant -i $indexLocation -l A -1 ${rawDataOutputDirectory}${i}_1.fastq.gz -2 ${rawDataOutputDirectory}${i}_2.fastq.gz --gcBias --validateMappings -o ${rawDataOutputDirectory}${i}_quant"
+  eval $salmonCall
+  
+  ##
+  starCall="$starLocation --runThreadN 12 --genomeDir ${referenceLocation} --readFilesIn ${rawDataOutputDirectory}${i}_1.fastq.gz ${rawDataOutputDirectory}${i}_2.fastq.gz --readFilesCommand zcat --sjdbGTFfile ${annotationLocation} --outFileNamePrefix ${rawDataOutputDirectory}starResults/${i}_ --outSAMtype BAM Unsorted"
+  eval $starCall
+  
+  ##
+  bamSortCall="$samtoolsLocation sort ${rawDataOutputDirectory}/starResults/${i}_Aligned.out.bam -o ${rawDataOutputDirectory}/starResults/${i}.sorted.bam"
+  eval $bamSortCall
+  
+  ##
+  bamIndexCall="$samtoolsLocation index ${rawDataOutputDirectory}/starResults/${i}.sorted.bam"
+  eval $bamIndexCall
+  
+  ##
+  coverageCall="bamCoverage -b ${rawDataOutputDirectory}/starResults/${i}.sorted.bam -o ${rawDataOutputDirectory}/starResults/${i}.chr11.bw --binSize 10 --region chr11 --normalizeUsing BPM --smoothLength 30 --centerReads -p 6"
+  eval $coverageCall
+done
+
+
+
+
+
